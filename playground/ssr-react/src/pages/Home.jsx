dd6cbb4fd3823bc324e3b145ed7aba0e8f544e6e
josetr <37419832+josetr@users.noreply.github.com>
2023-04-07 08:44:14 +0100

    alias working

--- playground/ssr-react/src/pages/Home.jsx
+++ playground/ssr-react/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import { addAndMultiply } from '../add'
 import { multiplyAndAdd } from '../multiply'
+import { Text } from "react-native";
+import { Text as PaperText } from "react-native-paper";
 
 export default function Home() {
   return (
     <>
       <h1>Home</h1>
+      <Text>Home</Text>
       <div>{addAndMultiply(1, 2, 3)}</div>
       <div>{multiplyAndAdd(1, 2, 3)}</div>
     </>
--- playground/ssr-react/vite.config.js
+++ playground/ssr-react/vite.config.js
@@ -1,10 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const extensions = [
+  '.web.tsx',
+  '.tsx',
+  '.web.ts',
+  '.ts',
+  '.web.jsx',
+  '.jsx',
+  '.web.js',
+  '.js',
+  '.css',
+  '.json',
+]
+
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: { include: ['react/jsx-dev-runtime'] },
+  optimizeDeps: {
+    include: ['react/jsx-dev-runtime'],
+    esbuildOptions: {
+      resolveExtensions: extensions,
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
   build: {
     minify: false,
+  },
+  resolve: {
+    extensions: extensions,
+    alias: {
+      'react-native': 'react-native-web',
+    },
   },
 })
